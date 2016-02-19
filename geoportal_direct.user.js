5a26ba4743863a60325ffcc54fc7be25936a2ec2
Paweł Pyrczak <pawel.pyrczak@kissdigital.pl>
2016-02-19 09:27:05 +0100

    converted all tabs to space

--- geoportal_direct.user.js
+++ geoportal_direct.user.js
@@ -2,14 +2,14 @@
 // @name            geoportal.gov.pl layers for WME without translating PROXY
 // @version         0.2.13.4
 // @description     Displays layers from geoportal.gov.pl in WME
-// @grant 			none
+// @grant           none
 // @include         https://*.waze.com/*/editor/*
 // @include         https://*.waze.com/editor/*
 // @include         https://*.waze.com/map-editor/*
 // @include         https://*.waze.com/beta_editor/*
 // @include         https://editor-beta.waze.com/*
-// @copyright		2013,2014+, Patryk Ściborek, Paweł Pyrczak
-// @run-at 			document-end
+// @copyright       2013,2014+, Patryk Ściborek, Paweł Pyrczak
+// @run-at          document-end
 // ==/UserScript==
 
 /**
@@ -20,20 +20,20 @@ function GEOPORTAL_bootstrap()
 {
     console.log("Geoportal: injecting code to page.");
     //use "dirty" but effective method with injection to document
-	var DLscript = document.createElement("script");
-	DLscript.textContent =''+
-	geoportal_run.toString()+'unsafeWindow=window; \n'+
+    var DLscript = document.createElement("script");
+    DLscript.textContent =''+
+    geoportal_run.toString()+'unsafeWindow=window; \n'+
         'geoportal_run() \n';
-	DLscript.setAttribute("type", "application/javascript");
-	document.body.appendChild(DLscript);
+    DLscript.setAttribute("type", "application/javascript");
+    document.body.appendChild(DLscript);
 }
 
 function geoportal_run() {
     GEOPORTAL = { ver: "0.2.13.4" };
     GEOPORTAL.init = function(w)
     {
-        console.log('Geoportal: start init '); 
-    
+        console.log('Geoportal: start init ');
+
         wms_service_orto="http://mapy.geoportal.gov.pl/wss/service/img/guest/ORTO/MapServer/WMSServer?"; // layer: Raster
         wms_service_orto_2="http://sdi.geoportal.gov.pl/wms_orto/wmservice.aspx?"; // layer: ORTOFOTO,ORTOFOTO_ISOK
         wms_service_prng="http://mapy.geoportal.gov.pl/wss/service/pub/guest/G2_PRNG_WMS/MapServer/WMSServer?dpi=130&"; // nazwy
@@ -41,15 +41,15 @@ function geoportal_run() {
         wms_bdot = "http://mapy.geoportal.gov.pl/wss/service/pub/guest/kompozycjaG2_TBD_WMS/MapServer/WMSServer?dpi=130&";
         var my_wazeMap = w;
         if (typeof my_wazeMap == undefined) my_wazeMap = unsafeWindow.Waze.map;
-         
+
         var epsg900913 = new unsafeWindow.OpenLayers.Projection("EPSG:900913");
-    	var epsg4326 =  new unsafeWindow.OpenLayers.Projection("EPSG:4326");
-    	var tileSizeG = new unsafeWindow.OpenLayers.Size(512,512);
-        
-    	ConvTo2180 = function(p) {
-            var D2R = 0.01745329251994329577; 
+        var epsg4326 =  new unsafeWindow.OpenLayers.Projection("EPSG:4326");
+        var tileSizeG = new unsafeWindow.OpenLayers.Size(512,512);
+
+        ConvTo2180 = function(p) {
+            var D2R = 0.01745329251994329577;
             var mlfn = function(e0, e1, e2, e3, phi) {
-      	        return (e0 * phi - e1 * Math.sin(2 * phi) + e2 * Math.sin(4 * phi) - e3 * Math.sin(6 * phi));
+                return (e0 * phi - e1 * Math.sin(2 * phi) + e2 * Math.sin(4 * phi) - e3 * Math.sin(6 * phi));
             }
             var contants = {
                 a: 6378137.0,
@@ -58,7 +58,7 @@ function geoportal_run() {
                 y0 : -5300000,
                 k0 : 0.9993,
                 init : function() {
-                    var D2R = 0.01745329251994329577; 
+                    var D2R = 0.01745329251994329577;
                     this.lon0 = 19.0 * D2R;
                     this.lat0 = 0 * D2R;
                     this.b = ((1.0 - 1.0 / this.rf) * this.a);
@@ -114,177 +114,177 @@ function geoportal_run() {
             p.lon = x;
             p.lat = y;
             return p;
-        };	
-        
+        };
+
         getUrl4326 = function (bounds) {
-    	    /* this function is modified Openlayer WMS CLASS part */
-    		/* Copyright (c) 2006-2013 by OpenLayers Contributors (see authors.txt for
-    		* full list of contributors). Published under the 2-clause BSD license.
-    		* See license.txt in the OpenLayers distribution or repository for the
-    		* full text of the license. */
-    		bounds = bounds.clone(); // Zrobione dlatego że tranformacja była dziedziczona do parenta i się sypało aż niemiło
-    		bounds = this.adjustBounds(bounds);
-    		
-    		var imageSize = this.getImageSize(bounds);
-    		var newParams = {};
-    		bounds.transform(this.epsg900913,this.epsg4326);
+            /* this function is modified Openlayer WMS CLASS part */
+            /* Copyright (c) 2006-2013 by OpenLayers Contributors (see authors.txt for
+            * full list of contributors). Published under the 2-clause BSD license.
+            * See license.txt in the OpenLayers distribution or repository for the
+            * full text of the license. */
+            bounds = bounds.clone(); // Zrobione dlatego że tranformacja była dziedziczona do parenta i się sypało aż niemiło
+            bounds = this.adjustBounds(bounds);
+
+            var imageSize = this.getImageSize(bounds);
+            var newParams = {};
+            bounds.transform(this.epsg900913,this.epsg4326);
             if (this.ep2180) {
-               	bounds = bounds.clone();
-               	a={lat: bounds.bottom , lon: bounds.right}
-               	b={lat: bounds.top, lon: bounds.left}
-               	a=this.ConvTo2180(a);
-               	b=this.ConvTo2180(b);
-              
-               	bounds.bottom = a.lat;
-               	bounds.right = a.lon;
-               	bounds.top = b.lat;
-               	bounds.left = b.lon;
+                bounds = bounds.clone();
+                a={lat: bounds.bottom , lon: bounds.right}
+                b={lat: bounds.top, lon: bounds.left}
+                a=this.ConvTo2180(a);
+                b=this.ConvTo2180(b);
+
+                bounds.bottom = a.lat;
+                bounds.right = a.lon;
+                bounds.top = b.lat;
+                bounds.left = b.lon;
             }
             // WMS 1.3 introduced axis order
-    		var reverseAxisOrder = this.reverseAxisOrder();
-    		newParams.BBOX = this.encodeBBOX ?
-    			bounds.toBBOX(null, reverseAxisOrder) :
-    			bounds.toArray(reverseAxisOrder);
-    		newParams.WIDTH = imageSize.w;
-    		newParams.HEIGHT = imageSize.h;
-    		var requestString = this.getFullRequestString(newParams);
-    		return requestString;
+            var reverseAxisOrder = this.reverseAxisOrder();
+            newParams.BBOX = this.encodeBBOX ?
+                bounds.toBBOX(null, reverseAxisOrder) :
+                bounds.toArray(reverseAxisOrder);
+            newParams.WIDTH = imageSize.w;
+            newParams.HEIGHT = imageSize.h;
+            var requestString = this.getFullRequestString(newParams);
+            return requestString;
         };
-            
+
         getFullRequestString4326 = function(newParams, altUrl) {
-    	    /* this function is modified Openlayer WMS CLASS part */
-    		/* Copyright (c) 2006-2013 by OpenLayers Contributors (see authors.txt for
-    		* full list of contributors). Published under the 2-clause BSD license.
-    		* See license.txt in the OpenLayers distribution or repository for the
-    		* full text of the license. */
-    		var mapProjection = this.map.getProjectionObject();
-    		var projectionCode = this.projection.getCode();
-    		var value = (projectionCode == "none") ? null : projectionCode;
-    		if (parseFloat(this.params.VERSION) >= 1.3) {
-    			this.params.CRS = value;
-    		} else {
+            /* this function is modified Openlayer WMS CLASS part */
+            /* Copyright (c) 2006-2013 by OpenLayers Contributors (see authors.txt for
+            * full list of contributors). Published under the 2-clause BSD license.
+            * See license.txt in the OpenLayers distribution or repository for the
+            * full text of the license. */
+            var mapProjection = this.map.getProjectionObject();
+            var projectionCode = this.projection.getCode();
+            var value = (projectionCode == "none") ? null : projectionCode;
+            if (parseFloat(this.params.VERSION) >= 1.3) {
+                this.params.CRS = value;
+            } else {
                 if (this.ep2180) {
                     this.params.SRS = "EPSG:2180"; //na sztywno najlepiej
                 } else {
                     this.params.SRS = "EPSG:4326"; //na sztywno najlepiej
                 }
-    		}
-    		
-    		if (typeof this.params.TRANSPARENT == "boolean") {
-    		    newParams.TRANSPARENT = this.params.TRANSPARENT ? "TRUE" : "FALSE";
-    		}
-    
-    		return unsafeWindow.OpenLayers.Layer.Grid.prototype.getFullRequestString.apply(
-    														this, arguments);
-    	}
-            
+            }
+
+            if (typeof this.params.TRANSPARENT == "boolean") {
+                newParams.TRANSPARENT = this.params.TRANSPARENT ? "TRUE" : "FALSE";
+            }
+
+            return unsafeWindow.OpenLayers.Layer.Grid.prototype.getFullRequestString.apply(
+                                                            this, arguments);
+        }
+
         var geop_orto = new unsafeWindow.OpenLayers.Layer.WMS(
             "Geoportal - ortofoto",
-    		wms_service_orto,
+            wms_service_orto,
             {
                 layers: "Raster",
                 format: "image/jpeg"
             },
             {
-    		    tileSize: tileSizeG,
-    			isBaseLayer: false,
-    			visibility: false,
-    			transitionEffect: "resize",
-    			uniqueName: "orto1",
-    			epsg900913: epsg900913,
-    			epsg4326: epsg4326,
-    			getURL: getUrl4326,
-    			ConvTo2180: ConvTo2180,
+                tileSize: tileSizeG,
+                isBaseLayer: false,
+                visibility: false,
+                transitionEffect: "resize",
+                uniqueName: "orto1",
+                epsg900913: epsg900913,
+                epsg4326: epsg4326,
+                getURL: getUrl4326,
+                ConvTo2180: ConvTo2180,
                 ep2180: false,
-    			getFullRequestString: getFullRequestString4326
-    		}
+                getFullRequestString: getFullRequestString4326
+            }
         );
-    	I18n.translations.en.layers.name["orto1"] = "Geoportal - ortofoto";
-    
+        I18n.translations.en.layers.name["orto1"] = "Geoportal - ortofoto";
+
         //geoportal_prng
         var geop_prng = new OpenLayers.Layer.WMS(
             "Geoportal - nazwy PRNG",
-    		wms_service_prng,
-    		{
-    		    layers: "Wies,Miasto",
+            wms_service_prng,
+            {
+                layers: "Wies,Miasto",
                 transparent: "true",
                 format: "image/png"
-    		},
+            },
             {
-    			tileSize: tileSizeG,
-    			isBaseLayer: false,
-    			visibility: false,
-    			uniqueName: "nazwy",
-    			epsg900913: epsg900913,
-    			epsg4326: epsg4326,
-    			getURL: getUrl4326,
-    			ConvTo2180: ConvTo2180,
+                tileSize: tileSizeG,
+                isBaseLayer: false,
+                visibility: false,
+                uniqueName: "nazwy",
+                epsg900913: epsg900913,
+                epsg4326: epsg4326,
+                getURL: getUrl4326,
+                ConvTo2180: ConvTo2180,
                 ep2180: false,
-    			getFullRequestString: getFullRequestString4326
-    		}
+                getFullRequestString: getFullRequestString4326
+            }
         );
         I18n.translations.en.layers.name["nazwy"] = "Geoportal - nazwy PRNG";
-       	
-    	/// ,Zabudowa_A,Przem_gosp_A,KomplKom_A
+
+        /// ,Zabudowa_A,Przem_gosp_A,KomplKom_A
         var geop_adresy2 = new OpenLayers.Layer.WMS(
             "Geoportal - adresy BDOT",
-    		wms_bdot,
-    		{
-    			layers: "BudHydro_A,Bud_A,BudSport_A,BudWys_A,BudCment_A,PktAdr_P,IUrzTechn_A,ZbTech_A",
-    			transparent: "true",
-    			format: "image/png32"
-    		},
+            wms_bdot,
             {
-    			tileSize: tileSizeG,
-    			isBaseLayer: false,
-    			visibility: false,
-    			uniqueName: "adresy2",
-    			epsg900913: epsg900913,
-    			epsg4326: epsg4326,
-    			getURL: getUrl4326,
-    			ConvTo2180: ConvTo2180,
+                layers: "BudHydro_A,Bud_A,BudSport_A,BudWys_A,BudCment_A,PktAdr_P,IUrzTechn_A,ZbTech_A",
+                transparent: "true",
+                format: "image/png32"
+            },
+            {
+                tileSize: tileSizeG,
+                isBaseLayer: false,
+                visibility: false,
+                uniqueName: "adresy2",
+                epsg900913: epsg900913,
+                epsg4326: epsg4326,
+                getURL: getUrl4326,
+                ConvTo2180: ConvTo2180,
                 ep2180: true,
-        		getFullRequestString: getFullRequestString4326
-    		}
+                getFullRequestString: getFullRequestString4326
+            }
         );
-    	I18n.translations.en.layers.name["adresy2"] = "Geoportal - adresy BDOT";
-        
+        I18n.translations.en.layers.name["adresy2"] = "Geoportal - adresy BDOT";
+
         console.log('Geoportal: adding layers');
-    	if(my_wazeMap.getLayersByName("Geoportal - orto").length == 0)
-    	{
-    		my_wazeMap.addLayer(geop_orto);
-    		my_wazeMap.addLayer(geop_prng);
-    		my_wazeMap.addLayer(geop_adresy2);
-    		console.log('Geoportal: layers added');
-    		this.OrtoTimer();
-    	}
+        if(my_wazeMap.getLayersByName("Geoportal - orto").length == 0)
+        {
+            my_wazeMap.addLayer(geop_orto);
+            my_wazeMap.addLayer(geop_prng);
+            my_wazeMap.addLayer(geop_adresy2);
+            console.log('Geoportal: layers added');
+            this.OrtoTimer();
+        }
     }
 
     GEOPORTAL.OrtoTimer = function() {
-	    setTimeout(function(){
-		    var a = unsafeWindow.Waze.map.getLayersBy("uniqueName","orto1");
-			if (a[0]) a[0].setZIndex(3);
-			unsafeWindow.Waze.map.getLayersBy("uniqueName","satellite_imagery").first().setZIndex(1); // mapy Googla
-			GEOPORTAL.OrtoTimer();
-		},500);
+        setTimeout(function(){
+            var a = unsafeWindow.Waze.map.getLayersBy("uniqueName","orto1");
+            if (a[0]) a[0].setZIndex(3);
+            unsafeWindow.Waze.map.getLayersBy("uniqueName","satellite_imagery").first().setZIndex(1); // mapy Googla
+            GEOPORTAL.OrtoTimer();
+        },500);
     }
 
     GEOPORTAL.initBootstrap = function() {
-	    try {
-		    if (undefined != typeof unsafeWindow.Waze.map.getLayersByName) {
-			    this.init(unsafeWindow.Waze.map);
-		    } else {
-    		    console.log("Geoportal: WME not initialized yet, trying again later.");
-    			setTimeout(function(){
-	    			GEOPORTAL.initBootstrap();
-		    	},1000);				
-		    }
-	    } catch (err) {
-		    console.log("Geoportal: WME not initialized yet, trying again later.");
-    		setTimeout(function(){
-	    		GEOPORTAL.initBootstrap();
-    	    },1000);			
-	    } 
+        try {
+            if (undefined != typeof unsafeWindow.Waze.map.getLayersByName) {
+                this.init(unsafeWindow.Waze.map);
+            } else {
+                console.log("Geoportal: WME not initialized yet, trying again later.");
+                setTimeout(function(){
+                    GEOPORTAL.initBootstrap();
+                },1000);
+            }
+        } catch (err) {
+            console.log("Geoportal: WME not initialized yet, trying again later.");
+            setTimeout(function(){
+                GEOPORTAL.initBootstrap();
+            },1000);
+        }
     }
     GEOPORTAL.initBootstrap();
 }
